9197941d50d8d552a139947816f6e158fd964048
Ruslan Pulatov <r.pulatov@solutionfactory.ru>
2022-03-22 16:21:42 +0700

    fix

--- src/feature/nginx.js
+++ src/feature/nginx.js
@@ -1,7 +1,7 @@
 const shell = require('shelljs')
 function nginxReload() {
   return new Promise((resolve, reject) => {
-    shell.exec('sudo systemctl reload nginx', (error, stdout, stderr) => {
+    shell.exec('sudo /bin/systemctl reload nginx', (error, stdout, stderr) => {
       if (error) {
         return reject(error)
       }
