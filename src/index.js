e12dec9883e725993b506b0c8a175a60a12fd243
Ruslan Pulatov <r.pulatov@solutionfactory.ru>
2022-03-22 12:43:14 +0700

    bugfix: forget require path

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const { updateSite } = require('./feature/tilda')
 const { nginxReload } = require('./feature/nginx')
